2ef7c8d7d35e9f5854bcc4e5e47da56b7e5bec0f
Mikołaj Stefaniak <48585522+Bobbik1@users.noreply.github.com>
2021-04-28 23:16:44 +0200

    Update irrigation.h

--- esphome/irrigation.h
+++ esphome/irrigation.h
@@ -6,6 +6,7 @@ bool scheduled_runtime(string);
 string update_next_runtime(string);
 
 bool scheduled_runtime(string time) {
+  if(time.size() < 5){ return false; }
   // Retrieve the current time.
   auto time_now = id(homeassistant_time).now();
   int time_hour = time_now.hour;
